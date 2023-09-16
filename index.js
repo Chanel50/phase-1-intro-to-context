c9813cc504f7e6bc3561a6cb284a68c5d1fd41f2
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-17 01:51:17 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-// Your code here
+function createEmployeeRecord(arr) {
+    return {
+      firstName: arr[0],
+      familyName: arr[1],
+      title: arr[2],
+      payPerHour: arr[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(arr) {
+    return arr.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(employee, dateTime) {
+    const [date, time] = dateTime.split(" ");
+    employee.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(time),
+      date: date
+    });
+    return employee;
+  }
+  
+  function createTimeOutEvent(employee, dateTime) {
+    const [date, time] = dateTime.split(" ");
+    employee.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(time),
+      date: date
+    });
+    return employee;
+  }
+  
+  function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find(event => event.date === date);
+    const timeOut = employee.timeOutEvents.find(event => event.date === date);
+    const hoursWorked = (timeOut.hour - timeIn.hour) / 100;
+    return hoursWorked;
+  }
+  
+  function wagesEarnedOnDate(employee, date) {
+    const hoursWorked = hoursWorkedOnDate(employee, date);
+    return hoursWorked * employee.payPerHour;
+  }
+  
+  function allWagesFor(employee) {
+    const datesWorked = employee.timeInEvents.map(event => event.date);
+    const totalWages = datesWorked.reduce((total, date) => {
+      return total + wagesEarnedOnDate(employee, date);
+    }, 0);
+    return totalWages;
+  }
+  
+  function findEmployeeByFirstName(employees, firstName) {
+    return employees.find(employee => employee.firstName === firstName);
+  }
+  
+  function calculatePayroll(employees) {
+    return employees.reduce((totalPayroll, employee) => {
+      return totalPayroll + allWagesFor(employee);
+    }, 0);
+  }
+  
